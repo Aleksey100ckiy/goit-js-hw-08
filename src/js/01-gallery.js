3c6935297cf9616fe92b52b34a7b297d79ff1232
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-01-07 17:50:47 +0200

    робить 1 ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+
+
+const images = galleryItems.reduce((acc,{preview, original, description}) => acc +`
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,'');
+list.insertAdjacentHTML('beforeend', images);
+
+list.addEventListener('click', onGalleryClick);
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'});
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+};
